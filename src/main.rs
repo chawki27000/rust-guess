98fbe832fc194dba9322cc719f0c13eec2c93c79
chawki benchehida <chawki27000@hotmail.com>
2018-04-13 01:01:04 +0100

    add several rust mechanism

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number !");
+
+    let secret_number = rand::thread_rng().gen_range(0, 101);
+
+    loop {
+        println!("Please input your guess : ");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("you guessed : {}", guess);
+        
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too smalll!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            } 
+        }
+    }
+
 }
